e991ea6ee33ff47d9cb819cd4ceea3450e363fc1
Tarik Oukaci <tarik.oukaci@gmail.com>
2023-12-14 15:26:30 +0100

    API ONNECT

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import Form from './connect/Form';
+import Register from './connect/Register';
 
 const Home = () => {
     return <>
         <h1>E LEAGUE</h1>
-        <Form/>
+        <Register/>
     </>
 }
 
