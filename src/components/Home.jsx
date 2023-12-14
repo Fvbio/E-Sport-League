67f1f0ecdbbb1a80c98a99f835f4cf356eeebe83
emilienbld <emilien.bld03@gmail.com>
2023-12-14 15:19:26 +0100

    j'espère que ca fonctionne

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
+import Tournament from './Tournament';
 import Form from './connect/Form';
+import '../assets/css/style.css';
 
 const Home = () => {
     return <>
         <h1>E LEAGUE</h1>
-        <Form/>
+        <Tournament/>
     </>
 }
 
